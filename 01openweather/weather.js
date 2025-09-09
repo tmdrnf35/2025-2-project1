c7969dd58c16a809c72294816c26a867d3437edc
tmdrnf35 <tmdrnf35@g.shingu.ac.kr>
2025-09-09 11:12:23 +0900

    init

--- 01openweather/weather.js
+++ 01openweather/weather.js
@@ -1,4 +1,4 @@
-weatherurl = "https://api.openweathermap.org/data/2.5/weather?q=Seoul&units=metric&appid=7d96bc5108f52b80e2d9075a369b9f35"
+weatherurl = "https://api.openweathermap.org/data/2.5/weather?q=Seoul&units=metric&appid=7d96bc5108f52b80e2d9075a369b9f35&lang=kr"
 
 
 function getweather(){
@@ -10,10 +10,15 @@ function getweather(){
 
     }).done(function(response){
 
-        alert("성공했습니다")
-        alert(" 온도는= " + response.main.temp + " 바람속도는= " +  response.wind.speed 
-        + " description= " + response.weather[0].description)
-        console.log(response)
+        // alert("성공했습니다")
+        // // alert(" 온도는= " + response.main.temp + " 바람속도는= " +  
+        // // response.wind.speed + " description= " + response.weather[0].description)
+        // console.log(response)
+
+        txtMSG.value =  "온도는=" + response.main.temp +
+         "\n 바람속도는= " +  response.wind.speed + 
+         "\n 날씨는 " + response.weather[0].description
+
     }).fail(function(error){
         alert("실패했습니다")
     });

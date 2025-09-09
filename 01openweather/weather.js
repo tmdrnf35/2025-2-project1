b3457e3b3bf545f443e617b1b5993c1d6c75a617
tmdrnf35 <tmdrnf35@g.shingu.ac.kr>
2025-09-09 10:55:00 +0900

    first commit

---
+++ 01openweather/weather.js
@@ -0,0 +1,22 @@
+weatherurl = "https://api.openweathermap.org/data/2.5/weather?q=Seoul&units=metric&appid=7d96bc5108f52b80e2d9075a369b9f35"
+
+
+function getweather(){
+    // alert("getweather 가 수행됨")
+    $.ajax({
+
+        type: "GET",
+        url: weatherurl
+
+    }).done(function(response){
+
+        alert("성공했습니다")
+        alert(" 온도는= " + response.main.temp + " 바람속도는= " +  response.wind.speed 
+        + " description= " + response.weather[0].description)
+        console.log(response)
+    }).fail(function(error){
+        alert("실패했습니다")
+    });
+}
+
+getweather()
